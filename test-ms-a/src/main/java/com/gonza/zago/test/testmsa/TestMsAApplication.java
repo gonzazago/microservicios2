2d72045cbecbced6d60d0002e777d732e221fdd0
Josue David <josue270193@gmail.com>
2018-05-03 11:04:45 -0300

    Correcciones

--- test-ms-a/src/main/java/com/gonza/zago/test/testmsa/TestMsAApplication.java
+++ test-ms-a/src/main/java/com/gonza/zago/test/testmsa/TestMsAApplication.java
@@ -3,43 +3,12 @@ package com.gonza.zago.test.testmsa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @EnableDiscoveryClient
-@RestController
+@SpringBootApplication
 public class TestMsAApplication {
-
-
-//	@Value("${myprops.alias:alias!}")
-//	String alias;
-//
-//	@Bean
-//	@RefreshScope
-//	public Service service(){
-//		return new  Service(props.getName());
-//	}
-
-	@RequestMapping("/")
-	public String home() {
-		return "Hello world";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestMsAApplication.class, args);
 	}
 }
-
-//@Data
-//@RequiredArgsConstructor
-//class Service{
-//	private final String name ;
-//	private Service(){name="UNKNOWN";}
-//}
-//
-//@Data
-//@ConfigurationProperties("myprops")
-//class MyProps{
-//	private  String name = "World";
-//}
--- test-ms-b/src/main/java/com/gonza/zago/test/testmsb/TestMsBApplication.java
+++ test-ms-b/src/main/java/com/gonza/zago/test/testmsb/TestMsBApplication.java
@@ -3,18 +3,10 @@ package com.gonza.zago.test.testmsb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
 public class TestMsBApplication {
-
-	@RequestMapping("/")
-	public String home() {
-		return  "latitud:2,23234 longitud:45.3222 ";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestMsBApplication.class, args);
